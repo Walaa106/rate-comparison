0a51658b0fe21581c9e69ef8696e2220951e06db
Walaa <walaa@yamsafer.me>
2017-08-06 18:27:56 +0300

    Finish the first era

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 (async() => {
 
-  const index = require('./source/index')
+  const index = require('./source_1/index')
 
   //- Read all cities that we have local hotels.
-  const cityIds = require('./source/city_ids.json')
+  const cityIds = require('./source_1/city_ids.json')
 
   //- Read rates and availability in the search socket.
   for (const [key, cityId] of Object.entries(cityIds)) {
